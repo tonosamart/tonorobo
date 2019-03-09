1367f8268869b0c70532e4153147b4b4989e8002
tonosamart <nishida1224@gmail.com>
2019-03-09 21:22:41 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,39 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\uf075" color=#ee6e73 block="とのさまロボ"
+namespace tonosamarobo {
+    //% blockId=init block="ロボきどう %v"
+    export function init(): void {
+        pins.servoWritePin(AnalogPin.P12, 140)
+        pins.servoWritePin(AnalogPin.P16, 30)
+        serial.redirect(
+            SerialPin.P2,
+            SerialPin.P0,
+            BaudRate.BaudRate9600
+        )
+        basic.pause(1000)
+        music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+        serial.writeLine("tonosamarobo kidou shimashita")
+    }
+
+    //% blockId=say block="ロボはなす %v"
+    export function say(text:string): void {
+        serial.writeLine(text);
+        basic.pause(500 * text.length + 100);
+    }
+
+    //% blockId=left_arm block="ロボひだりて %v"
+    export function leftArm(): void {
+        pins.servoWritePin(AnalogPin.P12, 30)
+        basic.pause(500)
+        pins.servoWritePin(AnalogPin.P12, 140)
+        basic.pause(500)
+    }
+
+    //% blockId=right_arm block="ロボみぎて %v"
+    export function rightArm(): void {
+        pins.servoWritePin(AnalogPin.P16, 140)
+        basic.pause(500)
+        pins.servoWritePin(AnalogPin.P16, 30)
+        basic.pause(500)
+    }
+    
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
+tonosamarobo.init();
+
+tonosamarobo.say("tesuto");
+
