9c5071bf9445d10be4765be2889ef9279dbdfdac
tonosamart <nishida1224@gmail.com>
2019-03-10 11:04:31 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ namespace tonosamarobo {
     //% blockId=say block="ロボはなす %v"
     export function say(text: string): void {
         serial.writeLine(text);
-        basic.pause(350 * text.length + 100);
+        basic.pause(300 * text.length + 100);
     }
 
     //% blockId=left_arm block="ロボひだりて %v"
